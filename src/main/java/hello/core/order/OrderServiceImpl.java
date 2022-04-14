ace75cc8dfafa0ab7d93c5c8773a1a55cfd29056
zeroh0 <umberto1900@naver.com>
2022-04-14 15:27:49 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원 정보 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인 정책
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+}
